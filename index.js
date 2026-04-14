ab6f453a82496ececd559520bda3039a8cea9e83
wesongaamanda <wesongaamanda@gmail.com>
2026-04-14 16:05:32 +0300

    add

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+require("datejs");
 
+function combineUsers(...args) {
+   const combinedObject = {
+      users: []
+   };
+
+   args.forEach(arr => {
+    combinedObject.users = [...combinedObject.users, ...arr];
+   });
+
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 
 module.exports = {
